6d227d62e5b37ee08c17b193fecf678091a501fe
Nguyen Tien Son <tienson19273@gmail.com>
2024-11-22 23:43:14 +0700

    Bug fixed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
-from src.button    import Button
-# from src.main_menu import Main_menu
+from src.main_menu import Main_menu
 pygame.init()
 pygame.mixer.init()
 pygame.mixer.music.load("assets/audios/background.wav")
@@ -11,50 +10,14 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Caro Game")
 
-# Create button
-start_normal  = "assets/graphics/buttons/start_button.png"
-start_hover   = "assets/graphics/buttons/start_button_hover.png"
-start_pressed = "assets/graphics/buttons/start_button_clicked.png"
-start_button  = Button(SCREEN_WIDTH/2-100, SCREEN_HEIGHT/3, 160, 60, start_normal, start_hover)
-
-exit_normal   = "assets/graphics/buttons/exit_button.png"
-exit_hover    = "assets/graphics/buttons/exit_button_hover.png"
-exit_pressed  = "assets/graphics/buttons/exit_button_clicked.png"
-exit_button   = Button(SCREEN_WIDTH/2-100, SCREEN_HEIGHT/2, 160, 60, exit_normal, exit_hover)
-
-background0_surface = pygame.image.load("assets/graphics/backgrounds/bgl0.png")
-background0_surface = pygame.transform.scale(background0_surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
-background1_surface = pygame.image.load("assets/graphics/backgrounds/bgl1.png")
-background1_surface = pygame.transform.scale(background1_surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
-background2_surface = pygame.image.load("assets/graphics/backgrounds/bgl2.png")
-background2_surface = pygame.transform.scale(background2_surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
-background3_surface = pygame.image.load("assets/graphics/backgrounds/bgl3.png")
-background3_surface = pygame.transform.scale(background3_surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
-
+main_menu = Main_menu(SCREEN_WIDTH,  SCREEN_HEIGHT)
 clock = pygame.time.Clock()
 
 # Main loop
 running = True
+screen_status = "main menu"
 while running:
-    screen.blit(background0_surface)
-    screen.blit(background1_surface)
-    screen.blit(background2_surface)
-    screen.blit(background3_surface)
+    if screen_status == "main menu":
+        running = main_menu.draw(screen)
     
-    for event in pygame.event.get():
-        start_button.event = event
-        exit_button.event = event
-        
-        if event.type == pygame.QUIT:
-            running = False
-        if start_button.is_pressed(event):
-            print("Start Game Button Clicked")
-        if exit_button.is_pressed(event):
-            running = False
-            print("Exit Game Button Clicked")
-    # Update the display
-    start_button.draw(screen)
-    exit_button.draw(screen)
-    pygame.display.update()
-
 pygame.quit()
--- src/main_menu.py
+++ src/main_menu.py
@@ -1,26 +1,26 @@
 import pygame
-from button import Button
+from .button import Button
 
 class Main_menu:
     def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT):
-        start_normal  = "../assets/graphics/buttons/start_button.png"
-        start_hover   = "../assets/graphics/buttons/start_button_hover.png"
-        start_button  = Button(SCREEN_WIDTH/2-100, SCREEN_HEIGHT/3, 160, 60, start_normal, start_hover)
+        self.start_normal  = "assets/graphics/buttons/start_button.png"
+        self.start_hover   = "assets/graphics/buttons/start_button_hover.png"
+        self.start_button  = Button(SCREEN_WIDTH/2-100, SCREEN_HEIGHT/3, 160, 60, self.start_normal, self.start_hover)
 
-        exit_normal   = "../assets/graphics/buttons/exit_button.png"
-        exit_hover    = "../assets/graphics/buttons/exit_button_hover.png"
-        exit_button   = Button(SCREEN_WIDTH/2-100, SCREEN_HEIGHT/2, 160, 60, exit_normal, exit_hover)
+        self.exit_normal   = "assets/graphics/buttons/exit_button.png"
+        self.exit_hover    = "assets/graphics/buttons/exit_button_hover.png"
+        self.exit_button   = Button(SCREEN_WIDTH/2-100, SCREEN_HEIGHT/2, 160, 60, self.exit_normal, self.exit_hover)
 
-        background0_surface = pygame.image.load("../assets/graphics/backgrounds/bgl0.png")
-        background0_surface = pygame.transform.scale(background0_surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
-        background1_surface = pygame.image.load("../assets/graphics/backgrounds/bgl1.png")
-        background1_surface = pygame.transform.scale(background1_surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
-        background2_surface = pygame.image.load("../assets/graphics/backgrounds/bgl2.png")
-        background2_surface = pygame.transform.scale(background2_surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
-        background3_surface = pygame.image.load("../assets/graphics/backgrounds/bgl3.png")
-        background3_surface = pygame.transform.scale(background3_surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.background0_surface = pygame.image.load("assets/graphics/backgrounds/bgl0.png")
+        self.background0_surface = pygame.transform.scale(self.background0_surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.background1_surface = pygame.image.load("assets/graphics/backgrounds/bgl1.png")
+        self.background1_surface = pygame.transform.scale(self.background1_surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.background2_surface = pygame.image.load("assets/graphics/backgrounds/bgl2.png")
+        self.background2_surface = pygame.transform.scale(self.background2_surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.background3_surface = pygame.image.load("assets/graphics/backgrounds/bgl3.png")
+        self.background3_surface = pygame.transform.scale(self.background3_surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
         
-    def draw(self, screen):
+    def draw(self, screen, running=True):
         screen.blit(self.background0_surface)
         screen.blit(self.background1_surface)
         screen.blit(self.background2_surface)
@@ -41,3 +41,5 @@ class Main_menu:
         self.start_button.draw(screen)
         self.exit_button.draw(screen)
         pygame.display.update()
+        
+        return running
